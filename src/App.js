bbd2a9c9bf826884daae585b726a64de39025b91
George Raymond <georgeraymond98@gmail.com>
2019-11-15 00:02:11 -0500

    mvp with magic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Form from "./components/Form";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Still working</h1>
+      <Form/>
     </div>
   );
 }
